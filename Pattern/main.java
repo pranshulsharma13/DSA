7ab7b3f24d4960e1d8d214c98f3918ae193a6d4a
pranshulsharma13 <pranshulsharmaalg@gmail.com>
2025-08-27 17:18:00 +0530

    Update main.java

--- Pattern/main.java
+++ Pattern/main.java
@@ -197,3 +197,160 @@
 //101
 //0101
 //10101
+
+
+/
+//public class Pattern{
+//    public static void main(String[] args){
+//        int n =5;
+//        //upper half
+//        for(int i = 0;i<=n;i++){
+//            //1st part
+//            for(int j = 1; j<=i ;j++) {
+//                System.out.print("*");
+//            }
+//            //spaces
+//            int spaces = 2*(n-i);
+//            for(int a=1;a<=spaces; a++){
+//                System.out.print(" ");
+//            }
+//
+//            //2nd part
+//            for(int b=1; b<=i ;b++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        //lower half
+//        for(int i = n ;i>=1 ;i--){
+//            //1st part
+//            for(int j = 1; j<=i ;j++) {
+//                System.out.print("*");
+//            }
+//            //spaces
+//            int spaces = 2*(n-i);
+//            for(int a=1;a<=spaces; a++){
+//                System.out.print(" ");
+//            }
+//
+//            //2nd part
+//            for(int b=1; b<=i ;b++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+
+//output
+//        *        *
+//        **      **
+//        ***    ***
+//        ****  ****
+//        **********
+//        **********
+//        ****  ****
+//        ***    ***
+//        **      **
+//        *        *
+
+
+//    public static void main(String[] args){
+//        int n = 5;
+//        for(int i = 1;i<=n;i++){
+//            //spaces
+//            for(int j = 1; j<=n-i ; j++){
+//                System.out.print(" ");
+//            }
+//            //stars
+//            for(int j=1;j<=n;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//output
+//                  *****
+//                *****
+//              *****
+//            *****
+//          *****
+
+//    public static void main(String[] args){
+//        int n = 5;
+//        for(int i = 1; i<=n;i++){
+//            for(int j = 1;j<=n-i;j++){
+//                System.out.print(" ");
+//
+//            }
+//            for(int j = i ; j >=1 ;j--){
+//                System.out.print(j);
+//            }
+//            for(int j = 2; j<=i; j++){
+//                System.out.print(j);
+//            }
+//
+//
+//
+//            System.out.println();
+//        }
+//
+//    }
+    //output
+//    1
+//   212
+//  32123
+// 4321234
+//543212345
+
+
+    public static void main(String[] args){
+        int n = 5;
+        for(int i = 1; i<=n;i++){
+            for(int j = 1;j<=n-i;j++){
+                System.out.print(" ");
+
+            }
+            for(int j = i ; j >=1 ;j--){
+                System.out.print("*");
+            }
+            for(int j = 2; j<=i; j++){
+                System.out.print("*");
+            }
+
+
+
+            System.out.println();
+        }
+
+        for(int i = n; i>=1;i--){
+            for(int j = 1;j<=n-i;j++){
+                System.out.print(" ");
+
+            }
+            for(int j = i ; j >=1 ;j--){
+                System.out.print("*");
+            }
+            for(int j = 2; j<=i; j++){
+                System.out.print("*");
+            }
+
+
+
+            System.out.println();
+        }
+
+    }
+    //output
+//    *
+//   ***
+//  *****
+// *******
+//*********
+//*********
+// *******
+//  *****
+//   ***
+//    *
+
+}
